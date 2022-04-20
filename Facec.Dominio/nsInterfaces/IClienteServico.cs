740ef767f5ba3698a5248748097d1043a64f0169
jenry <humbertopetita@gmail.com>
2022-04-19 22:37:14 -0300

    aula

---
+++ Facec.Dominio/nsInterfaces/IClienteServico.cs
@@ -0,0 +1,18 @@
+﻿using Facec.Dominio.nsEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facec.Dominio.nsInterfaces
+{
+    public interface IClienteServico
+    {
+        void Gravar(Cliente cliente);
+        void Excluir(Cliente excluir);
+        IEnumerable<Cliente> obter();
+
+
+    }
+}
